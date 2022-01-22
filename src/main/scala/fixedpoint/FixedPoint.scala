a9773fc890eaf6968c1cf64ee8bbb2184bc685a8
John Saylor <johnmsaylor@gmail.com>
2022-01-22 09:09:59 -0500

    return type required for public member

--- src/main/scala/fixedpoint/FixedPoint.scala
+++ src/main/scala/fixedpoint/FixedPoint.scala
@@ -8,7 +8,7 @@ class FixedPoint {
   def abs(x: Double): Double =
     if x > 0 then x else -x
 
-  def isCloseEnough(x: Double, y: Double) =
+  def isCloseEnough(x: Double, y: Double): Boolean =
     abs((x - y) / x) < tolerance
 
   def fixedPoint(f: Double => Double)(firstGuess: Double): Double =
@@ -22,5 +22,5 @@ class FixedPoint {
   def averageDamp(f: Double => Double)(x: Double): Double =
     (x + f(x)) / 2
   
-  def sqrt(x: Double) = fixedPoint(averageDamp(y => x / y))(1.0)
+  def sqrt(x: Double): Double = fixedPoint(averageDamp(y => x / y))(1.0)
 }
