0d7498ca38ec50b6cd599d8282268aad1ef461c7
John Saylor <johnmsaylor@gmail.com>
2022-01-29 09:44:21 -0500

    classes / objects

--- src/main/scala/data/Rational.scala
+++ src/main/scala/data/Rational.scala
@@ -6,9 +6,10 @@ object Rational {
     val hundred = new Rational(100, 1)
     val ten = new Rational(10, 1)
     val fifteen = new Rational(30, 2)
-    val two = new Rational(4, 2)
+    val two = new Rational(2, 1)
 
-    println(hundred.div(ten).sub(two))
+    println(ten.sub2(two).div(ten))
+//    println(hundred.div(ten).sub(two))
 //    println(hundred.mul(ten).mul(two))
 //    println(fifteen.sub(ten))
 //    println(two.sub(two))
@@ -19,11 +20,15 @@ class Rational(x: Int, y: Int) {
   def numerator: Int = x
   def denominator: Int = y
 
+  def neg: Rational = Rational(-numerator, denominator)
+
   def add(r: Rational): Rational =
     Rational(numerator * r.denominator + r.numerator * denominator, denominator * r.denominator)
 
   def sub(r: Rational): Rational =
     Rational(numerator * r.denominator - r.numerator * denominator, denominator * r.denominator)
+
+  def sub2(r: Rational): Rational = this.add(r.neg)
 
   def mul(r: Rational): Rational =
     Rational(numerator * r.numerator, denominator * r.denominator)
